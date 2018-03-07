05e6c0470234140fc44a562799bc8b1de5441bb1
Jonathan Hoang <6403049+Eleven-@users.noreply.github.com>
2018-03-06 20:35:50 -0800

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,34 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  
+  MatrixXd Hj(3,4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//TODO: YOUR CODE HERE 
+
+	//check division by zero
+	if((px+py)==0){
+	    cout << "Error";
+	}
+	else{
+	    float pxpy = px*px+py*py;
+	    Hj(0,0) = px/sqrt(pxpy);
+	    Hj(1,0) = -py/pxpy;
+	    Hj(0,1) = py/sqrt(pxpy);
+	    Hj(1,1) = px/pxpy;
+	    Hj(2,0) = py*(vx*py-vy*px)/sqrt(pxpy*pxpy*pxpy);
+	    Hj(2,1) = px*(vx*py-vy*px)/sqrt(pxpy*pxpy*pxpy);
+	    Hj(2,2) = px/sqrt(pxpy);
+	    Hj(2,3) = py/sqrt(pxpy);
+	}
+	//compute the Jacobian matrix
+
+	return Hj;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
