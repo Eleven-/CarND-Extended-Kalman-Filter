9cabf1d705f201199074b9355e357263ddcaab4d
Jonathan Hoang <6403049+Eleven-@users.noreply.github.com>
2018-03-06 20:46:01 -0800

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,16 +15,53 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
-  
-  MatrixXd Hj(3,4);
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	// ... your code here
+	if(estimations.size()==0){
+	    cout << "Error estimation vector size = 0";
+	    return rmse;
+	}
+	if(estimations.size()!=ground_truth.size()){
+	    cout << "Error estimation != ground truth size";
+	    return rmse;
+	}
+
+	//accumulate squared residuals
+	for(int i=0; i < estimations.size(); ++i){
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array()*residual.array();
+        rmse += residual;
+	}
+
+	//calculate the mean
+	// ... your code here
+	rmse = rmse/estimations.size();
+
+	//calculate the squared root
+	// ... your code here
+    rmse = rmse.array().sqrt();
+	//return the result
+	
+	return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+  TODO:
+    * Calculate a Jacobian here.
+  */
+  	MatrixXd Hj(3,4);
 	//recover state parameters
 	float px = x_state(0);
 	float py = x_state(1);
 	float vx = x_state(2);
 	float vy = x_state(3);
-
-	//TODO: YOUR CODE HERE 
-
+	
 	//check division by zero
 	if((px+py)==0){
 	    cout << "Error";
@@ -43,11 +80,4 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	//compute the Jacobian matrix
 
 	return Hj;
-}
-
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
 }
